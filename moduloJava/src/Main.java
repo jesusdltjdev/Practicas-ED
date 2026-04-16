0715855a3b0b4c067e0118b162ef4cce2ad94b34
jesusdltjdev <jesusdltj.dev@gmail.com>
2026-04-16 18:56:18 +0200

    Enhance prime number logic with non-prime list
    Added a new list for non-prime numbers and updated logic to populate both prime and non-prime lists.

--- moduloJava/src/Main.java
+++ moduloJava/src/Main.java
@@ -1,9 +1,13 @@
 import java.util.ArrayList;
 import java.util.List;
 
+// VAMOS A AÑADIR DESDE LA WEB NUEVAS LINEAS DE CODIGO
+
 public class Main {
     static void main(String[] args) {
         List<Integer> MilistadePrimos = new ArrayList<>() {};
+
+        List<Integer> MilistadeNoPrimos = new ArrayList<>() {};
 
         for (int i = 2; i <= 3000; i++) {
             boolean isPrimo = true;
@@ -15,10 +19,14 @@ public class Main {
             if (isPrimo) {
                 MilistadePrimos.add(i);
                 MilistadePrimos.getFirst();
+            } else{
+                MilistadeNoprimos.add(i);
             }
         }
 
         System.out.println(MilistadePrimos);
+        
+        System.out.println(MilistadePrimos.get(1));
 
 
     }
