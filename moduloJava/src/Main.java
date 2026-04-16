b3e77b3b1b531b15e67363c5b09be6fddaed364a
jesusdltjdev <jesusdltj.dev@gmail.com>
2026-04-16 18:27:27 +0200

    Se ha modificado Main.java para comprobar primos del 1 al 10000

--- moduloJava/src/Main.java
+++ moduloJava/src/Main.java
@@ -1,13 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.util.ArrayList;
+import java.util.List;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    static void main(String[] args) {
+        List<Integer> numerosPrimos = new ArrayList<>() {};
+
+        for (int i = 2; i <= 10000; i++) {
+            boolean isPrimo = true;
+            for (int j = 0; j < numerosPrimos.size(); j++ ) {
+                if (i % numerosPrimos.get(j) == 0) {
+                    isPrimo = false;
+                }
+            }
+            if (isPrimo) {
+                numerosPrimos.add(i);
+            }
+        }
+
+        System.out.println(numerosPrimos);
+        System.out.println(numerosPrimos.size());
+
     }
 }
